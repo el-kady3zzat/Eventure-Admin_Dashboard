260e31decace2425ca3894216d2202e8f61dc2ba
Qadi Ezzat <elkady.ezzat.ae@gmail.com>
2025-05-02 08:32:24 +0300

    Update Auth Design for Mobile Web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,14 +30,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "f05e432907a3edf80ff75d6deefddea7
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "eb5f30e9c5cf7ba5373a7c75b7df6c94",
+"flutter_bootstrap.js": "8c19f85bcfcb02e23d6f5acda1e8aecb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "08b49dd30ad257b369fcf85950639c9e",
 "/": "08b49dd30ad257b369fcf85950639c9e",
-"main.dart.js": "ff92a437ac7a9d9ad8c4e37f8bf9664f",
+"main.dart.js": "988e39952bb7e0ef8aa61b7337739f5e",
 "manifest.json": "01e86d1774378bd6d502548e2a51b163",
 "version.json": "4cad53583953f4d21af5d1c6c263c550"};
 // The application shell files that are downloaded before a service worker can
